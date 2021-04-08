982dac02837ffda80384a7afd5721853205053fb
DESKTOP-BHJPBEP\gravi <gravityfree01@gmail.com>
2021-04-08 21:26:48 +0900

    2번과제 풀이중..

---
+++ HD_pss_210408_2/HD_pss_210408_2/Source.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main() {
+	int i, j, k;
+	for ( i = 5; i > 1; i--) {
+		for ( k = 5; k >=i; k--) {
+			printf(" ");
+		}
+		for ( j = i; j <i ; k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+fgetc(stdin);
+}
